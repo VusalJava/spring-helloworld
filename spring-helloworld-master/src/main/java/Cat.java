40b1ee88fd8afffe2ce9b811f3d7edff24dca831
VusalJava <alievusal111@gmail.com>
2020-09-11 07:36:17 +0400

    Initial commit

---
+++ spring-helloworld-master/src/main/java/Cat.java
@@ -0,0 +1,11 @@
+
+public class Cat {
+    private String type;
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getType(){
+        return type;
+    }
+}
